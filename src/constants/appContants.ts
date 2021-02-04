c0619a530d1a6bc513fbb674f174feb0c8d37dde
Ilifilza Rusli <ilifilza@snappymob.com>
2021-02-05 02:37:43 +0800

    Add app assets and constants

---
+++ src/constants/appContants.ts
@@ -0,0 +1,4 @@
+/**
+ * API token for fakejson
+ */
+export const API_TOKEN = '61shyGutV7BWopN-mrJmag';
---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  turquoise: '#39c9bb',
+  lightcyan: '#E0FFFF',
+  grey: '#9E9E9E',
+  offwhite: '#DDDDDD',
+  darkGrey: '#464646',
+};
+
+export default Colors;
